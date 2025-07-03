9483e74e423f1f3be452e440c189d495e36e0f46
kuchazi-yy <1262280216@qq.com>
2025-07-03 21:58:05 +0800

    Site updated: 2025-07-03 21:58:05

--- js/title.js
+++ js/title.js
@@ -8,7 +8,7 @@ document.addEventListener('visibilitychange', function () {
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🥙鸡汤来喽～';
+    document.title = '🍟来喽～';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
